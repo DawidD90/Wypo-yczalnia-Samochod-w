986458598fedd54eb7596d72e2b81fe3eb3f1ea7
DawidD90 <dawid9013@onet.pl>
2023-03-28 20:25:18 +0200

    Hibernate connection setup pt2

--- src/main/java/com/example/carrental/repository/EmployeesRepository.java
+++ src/main/java/com/example/carrental/repository/EmployeesRepository.java
@@ -1,6 +1,5 @@
 package com.example.carrental.repository;
 
-import com.example.carrental.model.CarsModel;
 import com.example.carrental.model.EmployeesModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeesRepositroy extends JpaRepository<EmployeesModel, Long> {
+public interface EmployeesRepository extends JpaRepository<EmployeesModel, Long> {
 
     Page<EmployeesModel> findAllBy(Pageable pageable);
 
--- src/main/java/com/example/carrental/service/EmployeesService.java
+++ src/main/java/com/example/carrental/service/EmployeesService.java
@@ -1,9 +1,7 @@
 package com.example.carrental.service;
 
-import com.example.carrental.model.CarsModel;
 import com.example.carrental.model.EmployeesModel;
-import com.example.carrental.repository.CarsRepository;
-import com.example.carrental.repository.EmployeesRepositroy;
+import com.example.carrental.repository.EmployeesRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,28 +14,28 @@ import java.util.Optional;
 @Slf4j
 public class EmployeesService {
 
-    private final EmployeesRepositroy employeesRepositroy;
+    private final EmployeesRepository employeesRepository;
 
     public List<EmployeesModel> getAllEmployees(){
-        return employeesRepositroy.findAll();
+        return employeesRepository.findAll();
     }
 
     public void addEmployees(EmployeesModel employeesModel){
-        employeesRepositroy.save(employeesModel);
+        employeesRepository.save(employeesModel);
     }
 
     public void saveEditEmployees(EmployeesModel editEmployee) {
-        employeesRepositroy.save(editEmployee);
+        employeesRepository.save(editEmployee);
     }
 
     public void deleteEmployees(Long id) {
-        employeesRepositroy.deleteById(id);
+        employeesRepository.deleteById(id);
     }
 
     public EmployeesModel getEmployeeById(Long id) {
-        Optional<EmployeesModel> employee = employeesRepositroy.findById(id);
+        Optional<EmployeesModel> employee = employeesRepository.findById(id);
         if(employee.isPresent()){
-            return employeesRepositroy.findById(id).orElse(null);
+            return employeesRepository.findById(id).orElse(null);
         } else {
             log.info("brak");
             return null;
