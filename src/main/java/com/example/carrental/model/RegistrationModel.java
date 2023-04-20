3f691399ef003883481118f54c525c548498fe2c
DawidD90 <dawid9013@onet.pl>
2023-04-20 19:18:18 +0200

    Authentication update

---
+++ src/main/java/com/example/carrental/model/RegistrationModel.java
@@ -0,0 +1,29 @@
+package com.example.carrental.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class RegistrationModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "Username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToOne(mappedBy = "registrationModel")
+    private ClientModel clientModel ;
+}
